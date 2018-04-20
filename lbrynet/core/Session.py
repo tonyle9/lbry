168c0eebd7e34e63ed8f48e887c4a96e1af7b1c0
hackrush <atulshyan96@gmail.com>
2018-04-21 03:51:25 +0530

    convert wallet to be Component

--- lbrynet/core/Session.py
+++ lbrynet/core/Session.py
@@ -262,7 +262,6 @@ class Session(object):
 
         self.rate_limiter.start()
         d = self.blob_manager.setup()
-        d.addCallback(lambda _: self.wallet.start())
         d.addCallback(lambda _: self.blob_tracker.start())
         return d
 
